c422e8371706cad4231dc03f85308015952b3446
Vishwas poojary <dev@curiouslabs.in>
2019-03-30 00:12:52 +0530

    cars are getting displayed from the filtered array in cars.js to cars.html

--- assets/js/business/query/cars.js
+++ assets/js/business/query/cars.js
@@ -27,7 +27,7 @@ function get_the_location_and_day() {
     }
 
 
-    //initially filter the cars according to the location 
+    //initially filter the cars according to the location
     filter_cars_by_loc_and_day(str_location, frmt_day);
 }
 
@@ -59,7 +59,7 @@ function filter_cars_by_loc_and_day(location, day) {
                     })
                     if (is_day_found != undefined && is_day_found != null) {
                         return true;
-                    }else{
+                    } else {
                         push_to_unavailable(car)
                         false;
                     }
@@ -72,23 +72,122 @@ function filter_cars_by_loc_and_day(location, day) {
                 return false;
             }
         } else {
-            push_to_unavailable(car)
             return false;
         }
     });
 
     /**
-    * push the car to unavailable cars array 
+    * push the car to unavailable cars array
     */
     function push_to_unavailable(car) {
         car.is_available = false;
         unavailable_cars.push(car)
     }
 
+    //make single array
+    let all_cars = filtered_cars.concat(unavailable_cars);
+
     console.log("filtered cars on submit ", filtered_cars)
     console.log("unavailable cars ", unavailable_cars)
+    console.log("All cars", all_cars)
 
-    //make single array 
-    let all_cars = filtered_cars.concat(unavailable_cars);
+    all_cars.forEach(car => {
+        display_car(car)
+    });
+}
+
+/**
+ * This method is used to display the cars in the car.html page
+ */
+function display_car(car) {
+    //get the root reference
+    let root_element = document.getElementById("cars_cars_list");
+
+    //create car card
+    var car_card = document.createElement('div');
+    car_card.className = "single-car-wrap";
+
+    //card row
+    var card_row = document.createElement('div');
+    card_row.className = "row";
+    car_card.appendChild(card_row);
+
+    //single car thumbnell
+    var sc_thumb_out = document.createElement('div');
+    sc_thumb_out.className = "col-lg-5";
+    //thumb img
+    var sc_thumb_in = document.createElement('div');
+    sc_thumb_in.className = "car-list-thumb";
+    let car_img = document.createElement('img');
+    car_img.src = car.photo;
+    car_img.className="car-list-thumb";
+    //car_img.style.height="auto"
+    sc_thumb_in.appendChild(car_img);
+    sc_thumb_out.appendChild(sc_thumb_in);
+    card_row.appendChild(sc_thumb_out);
+
+    //car info col
+    var cr_inf_col = document.createElement('div');
+    cr_inf_col.className = "col-lg-7"
+    var cr_inf_dis_tbl = document.createElement('div');
+    cr_inf_dis_tbl.className = "display-table"
+    var cr_inf_dis_tbl_cell = document.createElement('div');
+    cr_inf_dis_tbl_cell.className = "display-table-cell"
+    //car info starts
+    var cr_inf = document.createElement('div');
+    cr_inf.className = "car-list-info"
+    //name
+    var div_name = document.createElement('h2');
+    div_name.innerHTML = car.name;
+    //fare
+    var div_fare = document.createElement('h5');
+    div_fare.innerHTML = "&#8377;" + car.price + "/Per day";
+    //misc info
+    var ul_misc = document.createElement('ul');
+    ul_misc.className = "car-info-list"
+    //seat
+    var li_seat = document.createElement('li');
+    li_seat.innerHTML = car.seats;
+    ul_misc.appendChild(li_seat)
+    //fuel
+    var li_fuel = document.createElement('li');
+    li_fuel.innerHTML = car.fuel_Type;
+    ul_misc.appendChild(li_fuel)
+    //gear
+    var li_gear = document.createElement('li');
+    li_gear.innerHTML = car.transmission;
+    ul_misc.appendChild(li_gear)
+
+    //locatioin
+    var car_location = document.createElement('p');
+    car_location.innerHTML = car.location;
+    car_location.style="float: right";
+
+    //location marker
+    var loc_marker = document.createElement('i');
+    loc_marker.className = "fa fa-map-marker"
+    loc_marker.style = "margin-right:5px; color: #ffd000;"
+    car_location.appendChild(loc_marker)
+
+    //availability
+    var p_avl = document.createElement('p');
+    p_avl.className = "rent-btn not-active"
+    p_avl.innerHTML = "NOT AVAILABLE"
+
+    //car info ends
+    cr_inf.appendChild(div_name)
+    cr_inf.appendChild(div_fare)
+    cr_inf.appendChild(ul_misc)
+    cr_inf.appendChild(car_location);
+    //check if car available
+    if(car.is_available==false){
+        cr_inf.appendChild(p_avl)
+    }
+    cr_inf_dis_tbl_cell.appendChild(cr_inf);
+    cr_inf_dis_tbl.appendChild(cr_inf_dis_tbl_cell);
+    cr_inf_col.appendChild(cr_inf_dis_tbl)
+    card_row.appendChild(cr_inf_col)
+
+    root_element.appendChild(car_card)
 }
 
